857ffd79ea66c117841eb82af81aa695f81154ad
Jomari Angel Jacinto <jomari.angel.jacinto@gmail.com>
2026-01-22 16:50:17 +0800

    Change route parameter from ':id' to ':scheduleId'
    Updated route parameter for updating flight status.

--- Backend/routes/schedule.js
+++ Backend/routes/schedule.js
@@ -15,11 +15,8 @@ router.post("/filter-by-status", verify, filterSchedulesByStatus);
 
 router.get("/", verify, verifyAdmin, getAllSchedules);
 
-// Route for searching flights (Best for the customer-facing search bar)
-// Example: /api/schedules/search?from=ID&to=ID&date=2024-05-20
 router.get('/search', searchFlights);
 
-// Update flight status (Active, Delayed, Cancelled, Finished)
-router.patch('/:id/status', verify, verifyAdmin, updateStatus);
+router.patch('/:scheduleId/status', verify, verifyAdmin, updateStatus);
 
 module.exports = router;
