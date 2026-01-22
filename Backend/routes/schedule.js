32a5a7ac9d7bbce8140ed41d6eab915b7c387c8f
Jomari Angel Jacinto <jomari.angel.jacinto@gmail.com>
2026-01-22 16:03:34 +0800

    Add verification middleware to getAllSchedules route

--- Backend/routes/schedule.js
+++ Backend/routes/schedule.js
@@ -13,16 +13,13 @@ router.post("/", verify, verifyAdmin, createSchedule);
 
 router.post("/filter-by-status", verify, filterSchedulesByStatus);
 
+router.get("/", verify, verifyAdmin, getAllSchedules);
+
 // Route for searching flights (Best for the customer-facing search bar)
 // Example: /api/schedules/search?from=ID&to=ID&date=2024-05-20
 router.get('/search', searchFlights);
 
-// Standard Routes
-router
-    .route('/')
-    .get(getAllSchedules) // Get all flights (for admin view)
-
 // Update flight status (Active, Delayed, Cancelled, Finished)
-router.patch('/:id/status', updateStatus);
+router.patch('/:id/status', verify, verifyAdmin, updateStatus);
 
 module.exports = router;
