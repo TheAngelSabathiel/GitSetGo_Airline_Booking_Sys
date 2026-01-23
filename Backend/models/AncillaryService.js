fe26ab593010a052601d74e04271fae32d1b9e2f
Jomari Angel Jacinto <jomari.angel.jacinto@gmail.com>
2026-01-23 12:59:50 +0800

    Update AncillaryService.js

--- Backend/models/AncillaryService.js
+++ Backend/models/AncillaryService.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const ancillaryServiceSchema = new mongoose.Schema({
-	airlineId : {
-		type : mongoose.Schema.Types.ObjectId,
-        ref : "Airline"
-	},
 	name : {
 		type : String,
 		required : [true, 'Name is required.']
