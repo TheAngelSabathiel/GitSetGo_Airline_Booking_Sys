3297dcadf6979f0a859a8c309454d74d59cd562f
roldanestobio <roldanestobio@gmail.com>
2026-01-25 00:30:59 +0800

    Bookings update to booking branch

--- Backend/index.js
+++ Backend/index.js
@@ -7,12 +7,16 @@ const cors = require("cors");
 const userRoutes = require("./routes/user");
 const bookingRoutes = require("./routes/booking");
 // const scheduleRoutes = require("./routes/schedule");
+
+// const bookingRoutes = require("./routes/booking");
+const scheduleRoutes = require("./routes/schedule");
 // const aircraftRoutes = require("./routes/aircraft");
-// const airportRoutes = require("./routes/airport");
+const airportRoutes = require("./routes/airport");
 // const flightSeatRoutes = require("./routes/flightSeat");
 // const fareClassRoutes = require("./routes/fareClass");
 // const ancillaryServiceRoutes = require("./routes/ancillaryService");
-// const codeRoutes = require("./routes/code");
+const codeRoutes = require("./routes/code");
+const promoRoutes = require("./routes/promo");
 
 // Google API Client
 const passport = require('passport');
@@ -32,13 +36,13 @@ app.use(express.urlencoded({extended : true}));
 app.use(cors());
 
 // Google Login
-// app.use(session({
-//     secret: process.env.clientSecret,
-//     resave: false,
-//     saveUninitialized: false
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(session({
+    secret: process.env.GOOGLE_clientSecret,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Database Connection
 mongoose.set("runValidators", true);
@@ -50,14 +54,20 @@ mongoose.connection.once("open", () => console.log("Now connected to MongoDB Atl
 //Routes Middlewares
 
 app.use("/users", userRoutes);
+
 app.use("/bookings", bookingRoutes);
 // app.use("/schedules", scheduleRoutes);
+
+// app.use("/bookings", bookingRoutes);
+app.use("/schedules", scheduleRoutes);
+
 // app.use("/aircrafts", aircraftRoutes);
-// app.use("/airports", airportRoutes);
+app.use("/airports", airportRoutes);
 // app.use("/flight-seats", flightSeatRoutes);
 // app.use("/fare-classes", fareClassRoutes);
 // app.use("/ancillary-services", ancillaryServiceRoutes);
-// app.use("/codes", codeRoutes);
+app.use("/codes", codeRoutes);
+app.use("/promos", promoRoutes);
 
 
 
